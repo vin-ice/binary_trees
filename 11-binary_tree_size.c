bc0dc1cd8606c2ba274e163c4879160602d961b4
vin-ice <57555841+vin-ice@users.noreply.github.com>
2023-03-03 23:47:42 +0300

    11-corr

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,8 +11,6 @@ size_t binary_tree_size(const binary_tree_t *tree)
     size_t size = 0;
     
     if (tree)
-        size = binary_tree_size(tree->left) +
-               binary_tree_size(tree->right) +
-               1;
+        size = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
     return (size);
 }
