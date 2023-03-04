6c5a6f556cbcc1c31b4c39f77372b4ae458c0f70
Vincent Wanjohi <vwan045@gmail.com>
2023-03-04 16:21:01 +0300

    Array to avl

--- 122-array_to_avl.c
+++ 122-array_to_avl.c
@@ -10,9 +10,12 @@
  */
 avl_t *array_to_avl(int *array, size_t size)
 {
-	unsigned int i;
+	size_t i;
 	avl_t *root = NULL;
 
+	if (!array || size == 0)
+		return (NULL);
+	root = avl_insert(&root, array[0]);
 	for (i = 0; i < size; i++)
 		avl_insert(&root, array[i]);
 
